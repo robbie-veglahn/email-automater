f34f6185a1f1733dac6a7153e2412439ada74ae6
robbie-veglahn <robbie.veglahn@gmail.com>
2021-10-30 15:03:48 -0400

    add interview_scheduler.py

---
+++ interview_scheduler.py
@@ -0,0 +1,19 @@
+import email_automator
+
+def send_virtual_invitation():
+    # TODO
+    return None
+
+def send_in_person_invitation():
+    # TODO
+    return None
+
+def send_daily_emails():
+    # TODO
+    return None
+
+def main():
+    # TODO
+    return None
+
+main()
